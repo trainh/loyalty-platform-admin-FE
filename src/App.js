ef1a1387075822bf125ba2f483fe68bd2fab0dcb
trainh <traitrantruon@gmail.com>
2022-05-26 17:37:50 +0700

    update to deploy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import ThemeProvider from './theme';
 import ScrollToTop from './components/ScrollToTop';
 import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
 
-// ----------------------------------------------------------------------
 
 export default function App() {
   return (
